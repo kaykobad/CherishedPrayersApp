70e9d5da936b8564be5c15f17eb117a923722eec
Kaykobad <kaykobadreza@gmail.com>
2021-01-26 15:10:17 +0600

    Add constants for the project

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const Color lightBackgroundColor = Color(0xFFFFFFFF);
+  static const Color darkBackgroundColor = Color(0xFF2D3238);
+
+  static const Color black = Color(0xFF2D3238);
+  static const Color lightPrimaryColor = Color(0xFF7A893B);
+}
---
+++ lib/constants/string_constants.dart
@@ -0,0 +1,3 @@
+class StringConstants {
+  static const String APP_NAME = "Cherished Prayers";
+}
