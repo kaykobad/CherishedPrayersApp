03b9d0ed7e83f78cc29d7755a711f5d56fdc7d51
Kaykobad <kaykobadreza@gmail.com>
2021-01-27 18:54:48 +0600

    Update themes

--- lib/theme/themes.dart
+++ lib/theme/themes.dart
@@ -10,28 +10,45 @@ class AppThemes {
     accentColor: Colors.lightGreenAccent,
     accentIconTheme: IconThemeData(color: Colors.white),
     hintColor: ColorConstants.hintGray,
+    fontFamily: "Poppins",
     buttonTheme: ButtonThemeData(
-      padding: EdgeInsets.all(12.0),
+      padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
       buttonColor: ColorConstants.lightPrimaryColor,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18))
     ),
     textTheme: TextTheme(
-      button: TextStyle(color: ColorConstants.white),
-      bodyText2: TextStyle(color: ColorConstants.black),
-      headline6: TextStyle(color: ColorConstants.black),
-      subtitle1: TextStyle(color: ColorConstants.black),
-      subtitle2: TextStyle(color: ColorConstants.gray),
+      headline4: TextStyle(fontSize: 42.0, fontWeight: FontWeight.w500),
+      headline5: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w500),
+      headline6: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+      bodyText1: TextStyle(fontSize: 12.0, fontWeight: FontWeight.normal),
+      bodyText2: TextStyle(fontSize: 14.0, fontWeight: FontWeight.normal),
+      button: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
     ),
     dividerColor: Colors.white54,
   );
 
   static final darkTheme = ThemeData(
-    primarySwatch: Colors.grey,
-    primaryColor: Colors.black,
+    primarySwatch: Colors.black45,
+    primaryColor: ColorConstants.darkPrimaryColor,
     brightness: Brightness.dark,
-    backgroundColor: const Color(0xFF212121),
-    accentColor: Colors.white,
+    backgroundColor: ColorConstants.darkBackgroundColor,
+    accentColor: Colors.black45,
     accentIconTheme: IconThemeData(color: Colors.black),
+    hintColor: ColorConstants.hintGray,
+    fontFamily: "Poppins",
+    buttonTheme: ButtonThemeData(
+        padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+        buttonColor: ColorConstants.lightPrimaryColor,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18))
+    ),
+    textTheme: TextTheme(
+      headline4: TextStyle(fontSize: 42.0, fontWeight: FontWeight.w500),
+      headline5: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w500),
+      headline6: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+      bodyText1: TextStyle(fontSize: 12.0, fontWeight: FontWeight.normal),
+      bodyText2: TextStyle(fontSize: 14.0, fontWeight: FontWeight.normal),
+      button: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+    ),
     dividerColor: Colors.black12,
   );
 }
