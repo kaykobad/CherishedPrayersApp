43251c804141a71168e5f1d72e6357f6ad390f96
Kaykobad <kaykobadreza@gmail.com>
2021-01-27 18:54:37 +0600

    Add fonts

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -9,5 +9,7 @@ class ColorConstants {
   static const Color gray = Color(0xFF8D8D8D);
   static const Color backGroundGray = Color(0xFFF0F0F0);
   static const Color hintGray = Color(0xFF86878B);
+
   static const Color lightPrimaryColor = Color(0xFF7A893B);
+  static const Color darkPrimaryColor = Color(0xFF2D3238);
 }
