4032867cb154e3086d5625b601365e13777c107a
Kaykobad <kaykobadreza@gmail.com>
2021-01-31 09:40:20 +0600

    Add dio provider for api call

---
+++ lib/data/network/api_endpoints.dart
@@ -0,0 +1,28 @@
+class ApiEndpoints {
+  static const String BASE_URL = "https://cp.mycodeacademia.com/api/";
+
+  // Auth URLs
+  static const String LOGIN = "account/auth/login/";
+  static const String LOGOUT = "account/auth/logout/";
+  static const String REGISTER = "account/auth/register/";
+  static const String CHANGE_PASSWORD = "account/auth/change-password/";
+  static const String RESET_PASSWORD = "account/auth/reset-password/";
+  static const String RESET_PASSWORD_CONFIRMATION = "account/auth/confirm-password-reset/";
+  static const String EMAIL_VERIFY = "account/auth/verify-email/";
+  static const String EMAIL_VERIFY_CONFIRMATION = "account/auth/confirm-email-verification/";
+
+  // Profile URLs
+  static const String GET_PROFILE = "account/profile/get-profile/";
+  static const String UPDATE_COUNTRY = "account/profile/update-country/";
+  static const String UPDATE_LANGUAGE = "account/profile/update-language/";
+  static const String UPDATE_RELIGION = "account/profile/update-religion/";
+  static const String UPDATE_PROFILE_PICTURE = "account/profile/update-profile-picture/";
+
+  // Feedback URLs
+  static const String POST_FEEDBACK = "feedback/post-feedback/";
+
+  // Common URLs
+  static const String GET_ALL_COUNTRIES = "configuration/all-countries/";
+  static const String GET_ALL_LANGUAGES = "configuration/all-languages/";
+  static const String GET_ALL_RELIGIONS = "configuration/all-religions/";
+}
---
+++ lib/data/network/dio_provider.dart
@@ -0,0 +1,80 @@
+import 'package:cherished_prayers/data/models/models.dart';
+import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+import 'api_endpoints.dart';
+
+class DioProvider {
+  final String _baseUrl = ApiEndpoints.BASE_URL;
+  Dio _dio;
+
+  DioProvider() {
+    _init();
+  }
+
+  _init() {
+    if (_dio == null) {
+      _dio = Dio();
+      _dio..options.baseUrl = _baseUrl;
+      _dio..options.connectTimeout = 10000;
+      _dio..options.receiveTimeout = 10000;
+      _dio..options.sendTimeout = 10000;
+      _dio.interceptors.add(PrettyDioLogger(
+          request: true,
+          requestHeader: true,
+          requestBody: true,
+          responseHeader: true,
+          responseBody: true,
+          error: true,
+          maxWidth: 90,
+          compact: true
+      ));
+    }
+  }
+
+  Future<dynamic> get(
+      String uri, {
+        Map<String, dynamic> queryParameters,
+        Options options,
+        CancelToken cancelToken,
+        ProgressCallback onReceiveProgress,
+      }) async {
+    try {
+      var response = await _dio.get(
+        uri,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response.data;
+    } on Exception catch (e) {
+      return ErrorModel("Error!", [e.toString()]).toJson();
+    }
+  }
+
+  Future<dynamic> post(
+      String uri, {
+        data,
+        Map<String, dynamic> queryParameters,
+        Options options,
+        CancelToken cancelToken,
+        ProgressCallback onSendProgress,
+        ProgressCallback onReceiveProgress,
+      }) async {
+    try {
+      var response = await _dio.post(
+        uri,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response.data;
+    } on Exception catch (e) {
+      return ErrorModel("Error!", [e.toString()]).toJson();
+    }
+  }
+}
