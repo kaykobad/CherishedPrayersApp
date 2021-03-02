70dbe2d4f3d2aec40c3e919a78d902adf9068b47
Kaykobad <kaykobadreza@gmail.com>
2021-03-02 21:04:47 +0600

    Add search in all thread page

--- lib/ui/chat_pages/all_threads_screen.dart
+++ lib/ui/chat_pages/all_threads_screen.dart
@@ -8,6 +8,7 @@ import 'package:cherished_prayers/ui/shared_widgets/avatar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 class AllThreadsPage extends StatefulWidget {
   @override
@@ -16,46 +17,94 @@ class AllThreadsPage extends StatefulWidget {
 
 class _AllThreadsPageState extends State<AllThreadsPage> {
   // TODO: Add menu popup functionality to delete message
-  // TODO: Add search thread functionality
   // TODO: Add delete thread functionality
   AppDataStorage _appDataStorage;
+  int _myId;
   List<QueryDocumentSnapshot> threadList = new List.from([]);
 
   @override
   void initState() {
     super.initState();
     _appDataStorage = RepositoryProvider.of<AppDataStorage>(context);
+    _myId = _appDataStorage.userData.id;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(12.0),
-          child: StreamBuilder(
-            stream: FirebaseFirestore.instance.collection('Threads')
-                .where("users", arrayContains: _appDataStorage.userData.id)
-                .orderBy("lastUpdateTimeStamp", descending: true)
-                .snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return Center(
-                  child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(ColorConstants.lightPrimaryColor),
-                  ),
-                );
-              } else {
-                threadList.addAll(snapshot.data.docs);
-                return ListView.builder(
-                  padding: EdgeInsets.all(10.0),
-                  itemBuilder: (context, index) => buildItem(snapshot.data.docs[index]),
-                  itemCount: snapshot.data.docs.length,
-                );
-              }
-            },
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(12.0),
+            child: StreamBuilder(
+              stream: FirebaseFirestore.instance.collection('Threads')
+                  .where("users", arrayContains: _appDataStorage.userData.id)
+                  .orderBy("lastUpdateTimeStamp", descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(ColorConstants.lightPrimaryColor),
+                    ),
+                  );
+                } else {
+                  threadList = snapshot.data.docs;
+                  return Column(
+                    children: [
+                      _getSearchWidget(),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        padding: EdgeInsets.all(10.0),
+                        itemBuilder: (context, index) => buildItem(snapshot.data.docs[index]),
+                        itemCount: snapshot.data.docs.length,
+                      ),
+                    ],
+                  );
+                }
+              },
+            ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _getSearchWidget() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+      child: TypeAheadField(
+        textFieldConfiguration: TextFieldConfiguration(
+          cursorColor: ColorConstants.lightPrimaryColor,
+          autofocus: false,
+          style: DefaultTextStyle.of(context).style.copyWith(
+            fontStyle: FontStyle.normal,
+          ),
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.search, size: 28),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            hintText: "Search a specific chat",
+            isDense: true,
+            fillColor: ColorConstants.gray,
+          ),
+        ),
+        suggestionsCallback: (pattern) async {
+          return threadList.where((element) {
+            Thread temp = Thread.fromJson(element.data());
+            return temp.getReceiverName(_myId).toLowerCase().contains(pattern.toLowerCase());
+          }).toList();
+        },
+        itemBuilder: (context, suggestion) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 6.0),
+            child: buildItem(suggestion),
+          );
+        },
+        onSuggestionSelected: (suggestion) {
+          NavigationHelper.push(context, ChatScreen(thread: Thread.fromJson(suggestion.doc)));
+        },
       ),
     );
   }
