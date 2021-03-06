b54664e902dab38a812fa2ce600b17ff5e254398
Kaykobad <kaykobadreza@gmail.com>
2021-03-06 12:17:31 +0600

    Add tab bar on feed screen

--- lib/ui/feed_pages/feed_screen.dart
+++ lib/ui/feed_pages/feed_screen.dart
@@ -1,9 +1,15 @@
+import 'dart:async';
+
 import 'package:cherished_prayers/constants/color_constants.dart';
+import 'package:cherished_prayers/data/models/models.dart';
 import 'package:cherished_prayers/data/network/api_endpoints.dart';
 import 'package:cherished_prayers/repository/app_data_storage.dart';
+import 'package:cherished_prayers/ui/feed_pages/feed_bloc/feed_bloc.dart';
+import 'package:cherished_prayers/ui/feed_pages/feed_bloc/feed_state.dart';
 import 'package:cherished_prayers/ui/shared_widgets/post_comment_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class FeedScreen extends StatefulWidget {
   @override
@@ -14,17 +20,50 @@ class _FeedScreenState extends State<FeedScreen> with SingleTickerProviderStateM
   AppDataStorage _appDataStorage;
   TabController _tabController;
   int _selectedIndex = 0;
+  String _authToken;
+  FeedBloc _feedBloc;
+  StreamSubscription<FeedState> _feedBlocListener;
+  List<PostResponse> _allFeedPosts = [];
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
     _appDataStorage = RepositoryProvider.of<AppDataStorage>(context);
+    _feedBloc = _appDataStorage.feedBloc;
+    _authToken = _appDataStorage.userData.authToken;
     _tabController.addListener(() {
       setState(() {
         _selectedIndex = _tabController.index;
         print(_selectedIndex);
       });
+    });
+    _listenFeedBloc();
+  }
+
+  @override
+  void dispose() {
+    _feedBlocListener?.cancel();
+    super.dispose();
+  }
+
+  _listenFeedBloc() {
+    _feedBlocListener = _feedBloc.listen((state) async {
+      if (state is LoadingFeedState) {
+        EasyLoading.show(
+          status: "Loading...",
+        );
+      } else if (state is ErrorState) {
+        await EasyLoading.dismiss();
+        EasyLoading.showError(
+            state.error.error + '\n' + state.error.details.join(' ')
+        );
+      } else if (state is MyFeedFetchedState) {
+        await EasyLoading.dismiss();
+        setState(() {
+          _allFeedPosts = state.allPosts.posts;
+        });
+      }
     });
   }
 
@@ -36,6 +75,7 @@ class _FeedScreenState extends State<FeedScreen> with SingleTickerProviderStateM
           child: Column(
             children: [
               PostCommentInputWidget(url: _appDataStorage.userData.avatar == null ? null : ApiEndpoints.URL_ROOT + _appDataStorage.userData.avatar, callback: onPostButtonPressed),
+              SizedBox(height: 10.0),
               _getTabBar(),
             ],
           ),
